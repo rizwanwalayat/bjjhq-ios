866fba4f7e4512f8cd158d6dc30a558c5b336ebf
Haider awan <ali.haid3r@phaedrasolutions.com>
2022-02-18 18:01:48 +0500

    correct error

--- BJJHQ/Base/Coordinator.swift
+++ BJJHQ/Base/Coordinator.swift
@@ -10,7 +10,7 @@ import UIKit
 protocol Coordinator {
     var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
-    func landedPage()
+    func landingPage()
     func homePage()
     func signUpPage()
     func signInPage()
